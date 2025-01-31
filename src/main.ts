11fc09ccd8941161d707389e7d964bfdf64d41ec
Marcin Mierzwa <marcin.mierzwa81@gmail.com>
2025-01-31 14:12:01 +0100

    update main.ts

--- src/main.ts
+++ src/main.ts
@@ -3,7 +3,21 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.enableCors();
-  await app.listen(process.env.PORT ?? 3000);
+  const allowedOrigins = [
+    'http://localhost:4200', // Lokalny Angular
+    'https://twoja-aplikacja-angular.vercel.app', // Angular na Vercel
+  ];
+
+  app.enableCors({
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'), false);
+      }
+    },
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    credentials: true,
+  });  await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
