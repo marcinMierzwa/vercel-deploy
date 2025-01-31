1b6412006065ca57db429374c3dd48279bcc641d
Marcin Mierzwa <marcin.mierzwa81@gmail.com>
2025-01-31 14:38:21 +0100

    cors update

--- src/main.ts
+++ src/main.ts
@@ -5,22 +5,11 @@ import { NestExpressApplication } from '@nestjs/platform-express';
 async function bootstrap() {
   const app = await NestFactory.create<NestExpressApplication>(AppModule);
 
-  const allowedOrigins = process.env.ALLOWED_ORIGINS
-    ? process.env.ALLOWED_ORIGINS.split(',')
-    : ['http://localhost:4200', 'https://twoja-aplikacja-angular.vercel.app'];
-
-  app.enableCors({
-    origin: (origin, callback) => {
-      if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-        callback(null, true);
-      } else {
-        callback(new Error('Not allowed by CORS'), false);
-      }
-    },
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
-    credentials: true,
-  });
-
+    app.enableCors({
+      origin: ['http://localhost:4200', 'https://twoja-aplikacja-angular.vercel.app'],
+      methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
+      credentials: true,
+    });
   await app.listen(process.env.PORT ?? 3000);
 }
 
